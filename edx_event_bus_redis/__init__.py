91f647c00e91c3bb0be56d4a3d1faa2563b96ad8
Ty Hob <bmesick@tcril.org>
2025-04-08 10:01:01 -0400

    chore: Bump version to 0.6.1

--- edx_event_bus_redis/__init__.py
+++ edx_event_bus_redis/__init__.py
@@ -5,4 +5,4 @@ Redis Streams implementation for the Open edX event bus.
 from edx_event_bus_redis.internal.consumer import RedisEventConsumer
 from edx_event_bus_redis.internal.producer import create_producer
 
-__version__ = "0.5.1"
+__version__ = "0.6.1"
